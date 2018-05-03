f2858a6d46b8b6dbab71cb1eb416d4b91d6551c0
maks <maks@coderiver.com.ua>
2018-05-03 17:07:34 +0300

    save

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,17 @@ const wrapper = $('.js-table-body');
 let data = [{name:'name1', value:'value1'},{name:'name2', value:'value2'}];
 
 $.each(data, function(i, item) {
-  wrapper.append(`<tr><td>${item.name}</td><td>${item.value}</td></tr>`);
+  wrapper.append(`<tr><td class="js-cell">${item.name}</td><td class="js-cell">${item.value}</td></tr>`);
 });
 
 
 $('.js-add-row').on('click', function() {
-  wrapper.append('<tr><td></td><td></td></tr>');
+  wrapper.append('<tr><td class="js-cell"></td><td class="js-cell"></td></tr>');
+});
+
+$('.js-cell').click(function() {
+  var cellValue = $(this).html();
+  var cellInput = `<input type="text" class="js-cell-input" value="${cellValue}" />`;
+  $(this).html(cellInput);
+  $(this).find('.js-cell-input').focus();
 });
